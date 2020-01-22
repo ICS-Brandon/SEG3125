f9d3997f11e30ddc75847ce66bee977e5e6b7988
ICS-Brandon <wilson.brandonj15@gmail.com>
2020-01-22 16:15:21 -0500

    Adding better styling and a page for questions to be displayed on

---
+++ Lab-1-Questionnaire/app/src/main/java/com/example/lab1questionnaire/QuestionScreen.java
@@ -0,0 +1,15 @@
+package com.example.lab1questionnaire;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuestionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.question_layout);
+
+    }
+}
