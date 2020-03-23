ac7ed60a0664de115b6286ace24f258ae1a772ed
ICS-Brandon <wilson.brandonj15@gmail.com>
2020-03-23 12:50:41 -0400

    Uploading project after implementation of majority of UI screens

---
+++ Wishy/app/src/main/java/com/example/wishy/CreateAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.wishy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.create_account);
+    }
+}
---
+++ Wishy/app/src/main/java/com/example/wishy/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.wishy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.home_layout);
+    }
+}
