639bca83e652f05a04ca2b560d069c509f21f691
ICS-Brandon <wilson.brandonj15@gmail.com>
2020-03-24 13:07:01 -0400

    Finished design of User Interface, functionality next

---
+++ Wishy/app/src/main/java/com/example/wishy/AddEditActivity.java
@@ -0,0 +1,14 @@
+package com.example.wishy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.add_edit_layout);
+    }
+}
---
+++ Wishy/app/src/main/java/com/example/wishy/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.wishy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.settings_layout);
+    }
+}
